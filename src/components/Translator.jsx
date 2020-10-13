2691de553ff0d5f0f5acc038647271afc0ed0d73
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-13 14:15:32 -0500

    added click handlers for button

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,31 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import rot13 from 'rot-thirteen';
 
  class Translator extends Component {
      state = {
          text: this.props.initialText || '',
+         translatedText: ''
      }
 
      _handleChange = (newText) => {
          this.setState({
              text: newText,
+             
+         })
+     }
+     _handleClick = () => {
+         const { text } = this.state;
+         const translated = rot13(text);
+         this.setState({
+             text: '',
+             translatedText: translated,
          })
      }
 
     render() {
-        const { text } = this.state
+        const { text, translatedText } = this.state
         return (
             <>
             <h4>Translator!</h4>
             <form>
                 <label>
                     Text to be translated!
-                    <input type="text" onChange={event => {
+                    <input value={text} type="text" onChange={event => {
                         console.log(event.target.value);
                         this._handleChange(event.target.value)
                     }}/>
                 </label>
+                <button type='button' onClick={this._handleClick}>Translate!</button>
             </form>
-            <p>{text}</p>
+            <p>Plain Text: {text}</p>
+            <p>Translated Text: {translatedText}</p>
             </>
         )
     }
